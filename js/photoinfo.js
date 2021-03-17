507f19d2edb5b7518b6c2716f5fcc1dc8527565f
OlegSpas <olegspasss@gmail.com>
2021-03-18 00:11:39 +0200

    update photocards

---
+++ js/photoinfo.js
@@ -0,0 +1,219 @@
+
+
+
+////First Photo/////////////////////////////////////////////////////
+const FirstPhotoButton = document.querySelector('.funiro_one img');
+const FirstScreenPhoto = document.querySelector('.info__first');
+const FirstCloseScreen = document.querySelector('.first-close__info');
+const FirstBackground = document.querySelector('.first__background')
+
+
+
+FirstPhotoButton.addEventListener('click', function(){
+    FirstScreenPhoto.classList.remove('OfflineScreen');
+    FirstScreenPhoto.classList.toggle('OnlineScreen');
+    FirstBackground.classList.toggle('visible__background');
+})
+
+
+FirstCloseScreen.addEventListener('click', function(){
+    FirstScreenPhoto.classList.remove('OnlineScreen');
+    FirstScreenPhoto.classList.toggle('OfflineScreen');
+    FirstBackground.classList.remove('visible__background');
+})
+
+
+////Two Photo/////////////////////////////////////////////////////
+
+const TwoCloseScreen = document.querySelector('.two-close__info')
+const TwoPhotoButton = document.querySelector('.funiro_two img');
+const TwoScreenPhoto = document.querySelector('.info__two');
+const TwoBackground = document.querySelector('.two__background')
+
+
+
+
+TwoPhotoButton.addEventListener('click', function(){
+    TwoScreenPhoto.classList.remove('OfflineScreen');
+    TwoScreenPhoto.classList.toggle('OnlineScreen');
+    TwoBackground.classList.add('visible__background');
+})
+
+
+TwoCloseScreen.addEventListener('click', function(){
+    TwoScreenPhoto.classList.remove('OnlineScreen');
+    TwoScreenPhoto.classList.toggle('OfflineScreen');
+    TwoBackground.classList.remove('visible__background');
+})
+
+
+////Three Photo/////////////////////////////////////////////////////
+
+const ThreeCloseScreen = document.querySelector('.three-close__info')
+const ThreePhotoButton = document.querySelector('.funiro_three img');
+const ThreeScreenPhoto = document.querySelector('.info__three');
+const ThreeBackground = document.querySelector('.three__background')
+
+
+
+
+ThreePhotoButton.addEventListener('click', function(){
+    ThreeScreenPhoto.classList.remove('OfflineScreen');
+    ThreeScreenPhoto.classList.toggle('OnlineScreen');
+    ThreeBackground.classList.add('visible__background');
+})
+
+
+ThreeCloseScreen.addEventListener('click', function(){
+    ThreeScreenPhoto.classList.remove('OnlineScreen');
+    ThreeScreenPhoto.classList.toggle('OfflineScreen');
+    ThreeBackground.classList.remove('visible__background');
+})
+
+
+
+////Four Photo/////////////////////////////////////////////////////
+
+const FourCloseScreen = document.querySelector('.four-close__info')
+const FourPhotoButton = document.querySelector('.funiro_four img');
+const FourScreenPhoto = document.querySelector('.info__four');
+const FourBackground = document.querySelector('.four__background')
+
+
+
+
+FourPhotoButton.addEventListener('click', function(){
+    FourScreenPhoto.classList.remove('OfflineScreen');
+    FourScreenPhoto.classList.toggle('OnlineScreen');
+    FourBackground.classList.add('visible__background');
+})
+
+
+FourCloseScreen.addEventListener('click', function(){
+    FourScreenPhoto.classList.remove('OnlineScreen');
+    FourScreenPhoto.classList.toggle('OfflineScreen');
+    FourBackground.classList.remove('visible__background');
+})
+
+
+////Five Photo/////////////////////////////////////////////////////
+
+const FiveCloseScreen = document.querySelector('.five-close__info')
+const FivePhotoButton = document.querySelector('.funiro_five img');
+const FiveScreenPhoto = document.querySelector('.info__five');
+const FiveBackground = document.querySelector('.five__background')
+
+
+
+
+FivePhotoButton.addEventListener('click', function(){
+    FiveScreenPhoto.classList.remove('OfflineScreen');
+    FiveScreenPhoto.classList.toggle('OnlineScreen');
+    FiveBackground.classList.add('visible__background');
+})
+
+
+FiveCloseScreen.addEventListener('click', function(){
+    FiveScreenPhoto.classList.remove('OnlineScreen');
+    FiveScreenPhoto.classList.toggle('OfflineScreen');
+    FiveBackground.classList.remove('visible__background');
+})
+
+
+
+
+////Six Photo/////////////////////////////////////////////////////
+
+const SixCloseScreen = document.querySelector('.six-close__info')
+const SixPhotoButton = document.querySelector('.funiro_six img');
+const SixScreenPhoto = document.querySelector('.info__six');
+const SixBackground = document.querySelector('.six__background');
+
+
+
+
+SixPhotoButton.addEventListener('click', function(){
+    SixScreenPhoto.classList.remove('OfflineScreen');
+    SixScreenPhoto.classList.toggle('OnlineScreen');
+    SixBackground.classList.add('visible__background');
+})
+
+
+SixCloseScreen.addEventListener('click', function(){
+    SixScreenPhoto.classList.remove('OnlineScreen');
+    SixScreenPhoto.classList.toggle('OfflineScreen');
+    SixBackground.classList.remove('visible__background');
+})
+
+
+////Seven Photo/////////////////////////////////////////////////////
+
+const SevenCloseScreen = document.querySelector('.seven-close__info')
+const SevenPhotoButton = document.querySelector('.funiro_seven img');
+const SevenScreenPhoto = document.querySelector('.info__seven');
+const SevenBackground = document.querySelector('.seven__background');
+
+
+
+
+SevenPhotoButton.addEventListener('click', function(){
+    SevenScreenPhoto.classList.remove('OfflineScreen');
+    SevenScreenPhoto.classList.toggle('OnlineScreen');
+    SevenBackground.classList.add('visible__background');
+})
+
+
+SevenCloseScreen.addEventListener('click', function(){
+    SevenScreenPhoto.classList.remove('OnlineScreen');
+    SevenScreenPhoto.classList.toggle('OfflineScreen');
+    SevenBackground.classList.remove('visible__background');
+})
+
+
+
+////Eight Photo/////////////////////////////////////////////////////
+
+const EightCloseScreen = document.querySelector('.eight-close__info')
+const EightPhotoButton = document.querySelector('.funiro_eight img');
+const EightScreenPhoto = document.querySelector('.info__eight');
+const EightBackground = document.querySelector('.eight__background');
+
+
+
+
+EightPhotoButton.addEventListener('click', function(){
+    EightScreenPhoto.classList.remove('OfflineScreen');
+    EightScreenPhoto.classList.toggle('OnlineScreen');
+    EightBackground.classList.add('visible__background');
+})
+
+
+EightCloseScreen.addEventListener('click', function(){
+    EightScreenPhoto.classList.remove('OnlineScreen');
+    EightScreenPhoto.classList.toggle('OfflineScreen');
+    EightBackground.classList.remove('visible__background');
+})
+
+
+////Nine Photo/////////////////////////////////////////////////////
+
+const NineCloseScreen = document.querySelector('.nine-close__info')
+const NinePhotoButton = document.querySelector('.funiro_nine img');
+const NineScreenPhoto = document.querySelector('.info__nine');
+const NineBackground = document.querySelector('.nine__background');
+
+
+
+
+NinePhotoButton.addEventListener('click', function(){
+    NineScreenPhoto.classList.remove('OfflineScreen');
+    NineScreenPhoto.classList.toggle('OnlineScreen');
+    NineBackground.classList.add('visible__background');
+})
+
+
+NineCloseScreen.addEventListener('click', function(){
+    NineScreenPhoto.classList.remove('OnlineScreen');
+    NineScreenPhoto.classList.toggle('OfflineScreen');
+    NineBackground.classList.remove('visible__background');
+})
