72bee758091ad9ff1d455c272f58960fd8e96a26
OlegSpas <olegspasss@gmail.com>
2021-03-19 20:53:42 +0200

    Update Gamb-Menu

--- js/gambmenu.js
+++ js/gambmenu.js
@@ -1,5 +1,18 @@
-$(function () {
-	$('.menu-open').click(function () {
-		$('.topmenu').toggleClass('show-menu')
-	})
-});
+
+
+const GamburgerMenu = document.querySelector('.menu-open');
+const FirstPart = document.querySelector('.first__part');
+const TwoPart = document.querySelector('.two__part');
+const ThreePart = document.querySelector('.three__part');
+const OnlineMenu = document.querySelector('.topmenu');
+
+
+
+GamburgerMenu.addEventListener('click', function(){
+	OnlineMenu.classList.toggle('show-menu');
+	GamburgerMenu.classList.toggle("menu-open__online")
+	FirstPart.classList.toggle('first__online');
+	TwoPart.classList.toggle('two__online');
+	ThreePart.classList.toggle('three__online');
+})
+
